d8abf814e4f31d98c1197132366b1caa7d357326
smenaria2 <git@stackblitz.com>
2024-11-20 08:34:34 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# shwasproject
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/smenaria2/shwasproject)
---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Hero from './components/Hero';
+import AuthorCard from './components/AuthorCard';
+import ContactForm from './components/ContactForm';
+import { authors } from './data/authors';
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 text-gray-100">
+      {/* Hero Section */}
+      <Hero />
+
+      {/* Authors Section */}
+      <div className="container mx-auto px-4 py-16">
+        <h2 className="text-3xl font-bold mb-8 text-center text-gray-100">Meet the Authors</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-6">
+          {authors.map((author) => (
+            <AuthorCard key={author.name} author={author} />
+          ))}
+        </div>
+      </div>
+
+      {/* Contact Form Section */}
+      <div className="container mx-auto px-4 py-16">
+        <ContactForm />
+      </div>
+    </div>
+  );
+}
